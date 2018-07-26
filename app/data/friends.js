76ef627edff8a1295ac03bc3f18627b48317e33c
Nivi Nagar <nivinagar@Nivis-MacBook.local>
2018-07-26 16:39:39 -0400

    intial commit

---
+++ app/data/friends.js
@@ -0,0 +1,59 @@
+var friendsList = [
+    {
+        name: "Channing Tatum",
+        picture: "https://scstylecaster.files.wordpress.com/2018/04/channing-tatum.jpg",
+        score: [3,2,4,5,5,3,3,4,5,1]
+    },
+    {
+        name: "Tom Cruise",
+        picture: "https://pixel.nymag.com/imgs/daily/vulture/2017/06/14/14-tom-cruise.w710.h473.jpg",
+        score: [2,4,5,3,1,1,2,1,4,5]
+    },
+    {
+        name: "Jennifer Lawerencec",
+        picture: "https://scstylecaster.files.wordpress.com/2018/03/jennifer-lawrence2.jpg",
+        score: [2,5,4,2,4,5,3,4,4,,2]
+    },
+    {
+        name: "Chris Evans",
+        picture: "https://i1.wp.com/www.usmagazine.com/wp-content/uploads/chris-evans-zoom-d474d1dd-424c-4ee3-9f83-697f44368ee2.jpg?crop=0px%2C62px%2C1480px%2C832px&resize=1600%2C900&ssl=1",
+        score: [4,4,4,4,4,4,4,4,4,4]
+    },
+    {
+        name: "Gal Gadot",
+        picture: "https://m.media-amazon.com/images/M/MV5BMjUzZTJmZDItODRjYS00ZGRhLTg2NWQtOGE0YjJhNWVlMjNjXkEyXkFqcGdeQXVyMTg4NDI0NDM@._V1_UY317_CR51,0,214,317_AL_.jpg",
+        score: [2,3,5,1,3,4,5,2,4,4]
+    },
+    {
+        name: "Henry Cavill",
+        picture: "https://hips.hearstapps.com/ell.h-cdn.co/assets/16/07/1455813161-elle-henrycavill.jpg",
+        score: [2,2,2,2,2,2,2,2,2,2]
+    },
+    {
+        name: "Ryan Reynolds",
+        picture: "http://www.bollywoodlife.com/wp-content/uploads/2018/07/1216-GQ-FERR02-01-Ryan-Reynolds-Deadpool-04.jpg",
+        score: [5,5,5,5,5,5,5,5,5,5]
+    },
+    {
+        name: "Blake Lively",
+        picture: "https://amp.thisisinsider.com/images/5ae86e9abd9671f9008b466d-750-563.jpg",
+        score: [2,3,2,3,2,3,2,3,3,2]
+    },
+    {
+        name: "Chris Pratt",
+        picture: "https://www1.cbn.com/sites/default/files/styles/original/public/media/standard/images/chrispratt_si_0.jpg?itok=zLGc_D_X",
+        score: [1,5,4,3,3,5,5,4,2,3]
+    },
+    {
+        name: "Chris Hemsworth",
+        picture: "https://s1.ibtimes.com/sites/www.ibtimes.com/files/styles/full/public/2018/04/22/chris-hemsworth.jpg",
+        score: [1,5,2,3,2,2,2,4,5,1]
+    },
+    {
+        name: "Emma Watson",
+        picture: "https://pbs.twimg.com/profile_images/972434879454613507/LFBBn38A_400x400.jpg",
+        score: [3,3,3,3,3,3,3,3,3,3]
+    }
+];
+
+module.exports = friendsList;
---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+//var friendsList = require("././friends");
+//console.log("friendsList");
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// app.get("/", function(req, res){
+//     res.send("Hello World");
+// })
+
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json())
+
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
+
+
+
+
+
+app.listen(PORT, function(){
+    console.log("App listening on PORT: " + PORT);
+});
+
